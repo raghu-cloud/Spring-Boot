c6673375286a7e059e98e14de196ab66179f3616
Raghu <raghupatil19@gmail.com>
2022-10-25 16:52:35 +0530

    Updates consumes and produces for topic controller

--- SpringInitializerProject-Data/src/main/java/com/main/topic/TopicController.java
+++ SpringInitializerProject-Data/src/main/java/com/main/topic/TopicController.java
@@ -3,6 +3,7 @@ package com.main.topic;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,14 +21,14 @@ public class TopicController {
 		return topicService.getAllTopics();
 	}
 
-	@RequestMapping("/topics/{id}")
+	@RequestMapping(method = RequestMethod.GET, value = "/topics/{id}")
 	public Topic getTopicById(@PathVariable("id") String id) {
 		return topicService.getTopicById(id);
 	}
 
-	@RequestMapping(method = RequestMethod.POST, value = "/topics")
-	public void addTopic(@RequestBody Topic t) {
-		topicService.addTopic(t);
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, value = "/topics")
+	public Topic addTopic(@RequestBody Topic t) {
+		return topicService.addTopic(t);
 	}
 
 	@RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
